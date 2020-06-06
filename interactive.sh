067494489c7a2adf54583ed8983f26be5afb3a21
halfdev <gusgodnd@naver.com>
2020-06-06 14:29:33 +0900

    -

---
+++ interactive.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+echo "What's your first name?"
+read firstname
+echo "Welcome, $firstname..! What's your last name"
+read lastname
+echo "Thanks, $firstname $lastname"
---
+++ process.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+echo "Hello $USER" # empty
+echo "I'm" $USER " and will be telling you about current process..!"
+echo "Running Process List"
+ps
